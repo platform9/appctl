cf7ad6de92b981be6288d7b5824f9f859f280ee7
AnirudhPokala <anirudh@platform9.com>
2021-11-30 15:19:19 +0530

    Modified the Appurl

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -1,6 +1,6 @@
 package constants
 
 var (
-	APPURL      = "http://127.0.0.1:6112/v1/apps/"
+	APPURL      = "http://127.0.0.1:6112/v1/apps"
 	TABLEFORMAT = "NAME | URL | IMAGE | NAMESPACE | CREATIONTIME"
 )
