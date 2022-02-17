c4a915dddd365d7b45b15359825b94eded71b986
Rohitrajak1807 <rrajak@platform9.com>
2022-02-17 12:05:51 +0530

    add PersistentPreRun hook to check for secrets

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,36 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/platform9/appctl/pkg/color"
+	"github.com/platform9/appctl/pkg/constants"
+
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
-
-var verbosity bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use: "appctl",
 	Long: `CLI to deploy & manage apps in Platform9 environment.
 Login first using "appctl login" to use available commands.`,
+	PersistentPreRun: ensureAppSecrets,
+}
+
+func ensureAppSecrets(cmd *cobra.Command, args []string) {
+	if cmd.Name() == "help" || cmd.Name() == "version" {
+		return
+	}
+	if constants.APPURL == "" || constants.DOMAIN == "" ||
+		constants.CLIENTID == "" || constants.GrantType == "" {
+		fmt.Println(color.Red("appctl secrets not set.\n",
+			"Please ensure all of APPURL, DOMAIN, CLIENTID, GRANT_TYPE and APPCTL_SEGMENT_WRITE_KEY are set during the build.",
+		))
+		os.Exit(1)
+	}
+
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
