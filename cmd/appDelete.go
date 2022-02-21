8c4aa8f6a555462d0b620c40eb8b6fab1f9eefa8
Rohit Rajak <39493074+Rohitrajak1807@users.noreply.github.com>
2022-02-21 17:50:32 +0530

    Cleanup code, remove unused. GROW-123 (#37)
    * add build instructions to readme
    
    * clean up cmd dir
    
    * cleanup pkg dir
    
    * remove unused struct, refactor checkServerDown
    
    * Added sample example commands, removed few semi-colons
    
    * Updated as per comments
    
    * even with master
    
    * Added sample example commands, removed few semi-colons
    
    * Upgraded new version
    
    * clean up cmd dir
    
    * indicate the use of auth0 in README, remove <> in GCR usage example
    
    * edit build instructions
    
    * correct comment for CLIENTID
    
    * correct usage for appDeploy
    
    * move optional build variables to bottom
    
    Co-authored-by: anirudhpokala <anirudh@platform9.com>

--- cmd/appDelete.go
+++ cmd/appDelete.go
@@ -11,7 +11,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var delete_example = `
+// usage example
+var deleteExample = `
   # Delete an app using app-name.
   appctl delete -n <appname>
 
@@ -24,30 +25,31 @@ var (
 	appCmdDelete = &cobra.Command{
 		Use:     "delete",
 		Short:   "Delete an existing app",
-		Example: delete_example,
+		Example: deleteExample,
 		Long:    `Delete an existing app`,
 		Run:     appCmdDeleteRun,
 	}
 )
 
+// command variables
 var (
 	// App name to delete.
-	AppNameDelete string
+	appNameDelete string
 	// To force delete an app.
 	force bool
 	//Choice to delete app
-	deleteApp string
+	deleteConfirmChoice string
 )
 
 func init() {
 	rootCmd.AddCommand(appCmdDelete)
-	appCmdDelete.Flags().StringVarP(&AppNameDelete, "app-name", "n", "", "Provide the name of app to be deleted")
+	appCmdDelete.Flags().StringVarP(&appNameDelete, "app-name", "n", "", "Provide the name of app to be deleted")
 	appCmdDelete.Flags().BoolVarP(&force, "force", "f", false, "To force delete an app")
 }
 
 // To delete an app by its name.
 func appCmdDeleteRun(cmd *cobra.Command, args []string) {
-	if AppNameDelete == "" {
+	if appNameDelete == "" {
 		fmt.Printf("App name not specified.\n")
 		return
 	}
@@ -59,49 +61,49 @@ func appCmdDeleteRun(cmd *cobra.Command, args []string) {
 			// To make sure delete the app
 			reader := bufio.NewReader(os.Stdin)
 			fmt.Printf("Are you sure you want to delete app (y/n)? ")
-			deleteApp, _ = reader.ReadString('\n')
-			deleteApp = strings.TrimSuffix(deleteApp, "\n")
-			deleteApp = strings.TrimSuffix(deleteApp, "\r")
+			deleteConfirmChoice, _ = reader.ReadString('\n')
+			deleteConfirmChoice = strings.TrimSuffix(deleteConfirmChoice, "\n")
+			deleteConfirmChoice = strings.TrimSuffix(deleteConfirmChoice, "\r")
 
 			// If response is other than "y" or "n"
-			if deleteApp != "y" && deleteApp != "n" {
+			if deleteConfirmChoice != "y" && deleteConfirmChoice != "n" {
 				fmt.Printf("Please enter correct input (y/n).\n")
 				continue
 			}
 			// To delete app if Yes
-			if deleteApp == "y" {
+			if deleteConfirmChoice == "y" {
 				// Validate app name.
-				if !constants.RegexValidate(AppNameDelete, constants.ValidAppNameRegex) {
+				if !constants.RegexValidate(appNameDelete, constants.ValidAppNameRegex) {
 					fmt.Printf("Invalid app name.\n")
 					return
 				}
-				errapi := appManageAPI.DeleteApp(AppNameDelete)
+				errapi := appManageAPI.DeleteApp(appNameDelete)
 				if errapi != nil {
 					fmt.Printf("%v", errapi)
 					return
 				}
-				fmt.Printf("Successfully deleted the app: %v\n", AppNameDelete)
+				fmt.Printf("Successfully deleted the app: %v\n", appNameDelete)
 				break
 			}
 			// To stop delete app process if No
-			if deleteApp == "n" {
+			if deleteConfirmChoice == "n" {
 				fmt.Printf("You have cancelled the app deletion activity!!\n")
 				break
 			}
 		}
 	} else {
 		// Validate app name.
-		if !constants.RegexValidate(AppNameDelete, constants.ValidAppNameRegex) {
+		if !constants.RegexValidate(appNameDelete, constants.ValidAppNameRegex) {
 			fmt.Printf("Invalid app name.\n")
 			return
 		}
 		// If force delete an app.
-		errapi := appManageAPI.DeleteApp(AppNameDelete)
+		errapi := appManageAPI.DeleteApp(appNameDelete)
 		if errapi != nil {
 			fmt.Printf("%v", errapi)
 			return
 		}
-		fmt.Printf("Successfully deleted the app: %v\n", AppNameDelete)
+		fmt.Printf("Successfully deleted the app: %v\n", appNameDelete)
 	}
 
 }
