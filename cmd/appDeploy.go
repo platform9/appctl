f7a3fdd1d1e09bd31e28698e69e742dee7b55196
Rohitrajak1807 <rrajak@platform9.com>
2022-02-21 13:10:50 +0530

    correct usage for appDeploy

--- cmd/appDeploy.go
+++ cmd/appDeploy.go
@@ -31,7 +31,7 @@ var deployExample = `
   	  appctl deploy -n <appname> -i <aws_account_id>.dkr.ecr.<region>.amazonaws.com/<image>:<tag> -u AWS -P <Password obtained from AWS CLI>
 
   	  # Sample command to deploy an app from a GCR private registry path
-  	  appctl deploy -n <appname> -i gcr.io/<GCP_projectID>/<image> -u oauth2_accesstoken -P <Token obtained from gcloud CLI>
+  	  appctl deploy -n <appname> -i gcr.io/<GCP_projectID>/<image> -u oauth2accesstoken -P <Token obtained from gcloud CLI>
 
 
   # Deploy an app using app-name and container image, and pass environment variables.
